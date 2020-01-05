15584880de97006e6197b128e32b96b34ec0b0c0
Hemant Vetal <hemantvetal813@gmail.com>
2020-01-05 15:53:04 +0530

    express sessions and cookies

--- Middleware/Authentication/BasicAuth.js
+++ Middleware/Authentication/BasicAuth.js
@@ -1,18 +1,31 @@
 module.exports = function basicAuth(req, res, next) {
-  const authHeader = req.headers.authorization;
-  if (!authHeader) {
-    res.status(401).send("Supply authentication headers");
-    const err = new Error("Supply authentication headers");
-    return next(err);
-  }
-  let auth = new Buffer.from(authHeader.split(" ")[1], "base64")
-    .toString()
-    .split(":");
-  let user = auth[0];
-  let password = auth[1];
-  if (user == "admin" && password == 123) {
-    next();
+  // if (!req.signedCookies.user) {
+  if (!req.session.user) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+      res.status(401).send("Supply authentication headers");
+      const err = new Error("Supply authentication headers");
+      return next(err);
+    }
+    let auth = new Buffer.from(authHeader.split(" ")[1], "base64")
+      .toString()
+      .split(":");
+    let user = auth[0];
+    let password = auth[1];
+    //hardcoded
+    if (user == "admin" && password == 123) {
+      // res.cookie("user", "admin", { signed: true });
+      req.session.user = "admin";
+      next();
+    } else {
+      res.send("Wrong username or password");
+    }
   } else {
-    res.send("Wrong username or password");
+    // if (req.signedCookies.user == "admin") {
+    if (req.session.user == "admin") {
+      next();
+    } else {
+      res.send("Wrong username or password in cookies");
+    }
   }
 };
--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-const expressSession = require("express-session");
-
+const helmet = require("helmet");
+var cookieParser = require("cookie-parser");
 const BasicAuth = require("./Middleware/Authentication/BasicAuth");
-const cookie = require("cookie-parser");
 var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
-var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 const mongoose = require("mongoose");
 const config = require("config");
+const session = require("express-session");
+const FileStore = require("session-file-store")(session);
 
 mongoose
   .connect("mongodb://localhost/vidly2", {
@@ -24,6 +24,8 @@ mongoose
 var app = express();
 // console.log(config.get("vidly_password"));
 
+//security  module
+app.use(helmet());
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
@@ -31,9 +33,18 @@ app.set("view engine", "pug");
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+// app.use(cookieParser("813"));
+app.use(
+  session({
+    name: session,
+    secret: "813",
+    saveUninitialized: false,
+    resave: false,
+    store: new FileStore()
+  })
+);
 
-// app.use(BasicAuth);//uncomment to use basic Auth include authorization header while passing req
+app.use(BasicAuth); //uncomment to use basic Auth include authorization header while passing req
 
 app.use(express.static(path.join(__dirname, "public")));
 
